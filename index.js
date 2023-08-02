f3db3f928e30900ca4b0337292813653e3316ec1
muhammadnaeemtahir <m.naeemtahir@hotmail.com>
2023-08-03 00:53:46 +0500

    Date API endpoint v1.0

--- index.js
+++ index.js
@@ -2,31 +2,48 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+// date API endpoint
+app.get("/api/:date?", (req, res) => {
+  const inputDate = req.params.date;
+  let timeStamp = null;
 
+  if (!inputDate) {
+    timeStamp = new Date();
+  } else if (!isNaN(inputDate)) {
+    timeStamp = new Date(parseInt(inputDate));
+  } else {
+    timeStamp = new Date(inputDate);
+  }
+
+  return res.json({
+    unix: timeStamp.getTime(),
+    utc: timeStamp.toUTCString(),
+  });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`http://localhost:${process.env.PORT}`);
 });
